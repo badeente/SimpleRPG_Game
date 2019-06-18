568c2136542009290f68d3d100fd3308fbff1c60
GT\harda <andreas.harder@gigatronik.com>
2019-06-18 10:06:22 +0200

    - Refactor some Classes - Implement GameItem, Weapon and ItemFactory

---
+++ SOSCSRPG/Engine/Models/Weapon.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Engine.Models
+{
+    public class Weapon : GameItem
+    {
+        public int MinimumDamge { get; set; }
+        public int MaximumDamge { get; set; }
+
+        public Weapon(int itemTypeId, string name, int price, int minDamage, int maxDamage) 
+            : base(itemTypeId, name, price)
+        {
+            MinimumDamge = minDamage;
+            MaximumDamge = maxDamage;
+        }
+
+
+        public new Weapon Clone()
+        {
+            return new Weapon(ItemTypeId, Name, Price, MinimumDamge, MaximumDamge);
+        }
+    }
+}
--- SOSCSRPG/Engine/ViewModels/GameSession.cs
+++ SOSCSRPG/Engine/ViewModels/GameSession.cs
@@ -7,7 +7,7 @@ using Engine.Factories;
 
 namespace Engine.ViewModels
 {
-    public class GameSession : INotifyPropertyChanged
+    public class GameSession : BaseNotificationClass
     {
         private Location _currentLocation;
         
@@ -19,14 +19,30 @@ namespace Engine.ViewModels
             set
             {
                 _currentLocation = value;
-                OnPropertyChanged("CurrentLocation");
-                OnPropertyChanged("HasLocationToNorth");
-                OnPropertyChanged("HasLocationToEast");
-                OnPropertyChanged("HasLocationToWest");
-                OnPropertyChanged("HasLocationToSouth");
+                OnPropertyChanged(nameof(CurrentLocation));
+                OnPropertyChanged(nameof(HasLocationToNorth));
+                OnPropertyChanged(nameof(HasLocationToEast));
+                OnPropertyChanged(nameof(HasLocationToWest));
+                OnPropertyChanged(nameof(HasLocationToSouth));
             }
         }
         public World CurrentWorld { get; set; }
+
+        public GameSession()
+        {
+            CurrentPlayer = new Player
+                            {
+                                Name = "Scott",
+                                CharacterClass = "Fighter",
+                                HitPoints = 10,
+                                Gold = 1000000,
+                                ExperiencePoints = 0,
+                                Level = 1
+                            };
+            CurrentWorld = WorldFactory.CreateWorld();
+            CurrentLocation = CurrentWorld.LocationAt(0, 0);
+        }
+
         public bool HasLocationToNorth
         {
             get
@@ -56,43 +72,33 @@ namespace Engine.ViewModels
             }
         }
 
-        public GameSession()
-        {
-            CurrentPlayer = new Player();
-            CurrentPlayer.Name = "Scott";          
-            CurrentPlayer.CharacterClass = "Fighter";
-            CurrentPlayer.HitPoints = 10;
-            CurrentPlayer.Gold = 1000000;
-            CurrentPlayer.ExperiencePoints = 0;
-            CurrentPlayer.Level = 1;
-
-            WorldFactory factory = new WorldFactory();
-            CurrentWorld = factory.CreateWorld();
-            CurrentLocation = CurrentWorld.LocationAt(0, 0);
-        }
-
         public void MoveNorth()
         {
-            CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate, CurrentLocation.YCoordinate + 1);
+            if (HasLocationToNorth)
+            {
+                CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate, CurrentLocation.YCoordinate + 1);
+            }
         }
         public void MoveWest()
         {
-            CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate - 1, CurrentLocation.YCoordinate);
+            if (HasLocationToWest)
+            {
+                CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate - 1, CurrentLocation.YCoordinate);
+            }
         }
         public void MoveEast()
         {
-            CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate + 1, CurrentLocation.YCoordinate);
+            if (HasLocationToEast)
+            {
+                CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate + 1, CurrentLocation.YCoordinate);
+            }  
         }
         public void MoveSouth()
         {
-            CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate, CurrentLocation.YCoordinate - 1);
-        }
-
-        public event PropertyChangedEventHandler PropertyChanged;
-
-        protected virtual void OnPropertyChanged(string propertyName)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            if (HasLocationToSouth)
+            {
+                CurrentLocation = CurrentWorld.LocationAt(CurrentLocation.XCoordinate, CurrentLocation.YCoordinate - 1);
+            }  
         }
     }
 }
